16addd3a482e190972351101e5cf3ad4512685b9
Waseem Usman <waseem.usman17@gmail.com>
2020-09-30 18:49:23 +0500

    Client Review Section

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$('.crs-slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    dots:false
+});
+$( ".crs-slider .owl-next").html('<i class="fa fa-angle-right" aria-hidden="true"></i>');
+$( ".crs-slider .owl-prev").html('<i class="fa fa-angle-left" aria-hidden="true"></i>');
